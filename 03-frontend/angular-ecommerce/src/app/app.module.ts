7aafc1008b9b7b0b0c0504a5c1e7e2d5daef76aa
LefterisCoder <107108837+LefterisCoder@users.noreply.github.com>
2023-06-23 16:29:22 +0300

    Changed the Cart status so it is dynamic

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -12,6 +12,8 @@ import {SearchComponent} from './components/search/search.component';
 import {ProductDetailsComponent} from './components/product-details/product-details.component';
 
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import {CartItem} from "./common/cart-item";
 
 const routes: Routes = [
     {path: 'products/:id', component: ProductDetailsComponent},
@@ -29,13 +31,14 @@ const routes: Routes = [
         ProductListComponent,
         ProductCategoryMenuComponent,
         SearchComponent,
-        ProductDetailsComponent
+        ProductDetailsComponent,
+        CartStatusComponent,
     ],
     imports: [
         RouterModule.forRoot(routes),
         BrowserModule,
         HttpClientModule,
-        NgbModule
+        NgbModule,
     ],
     providers: [ProductService],
     bootstrap: [AppComponent]
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from 'src/app/services/product.service';
 import {Product} from 'src/app/common/product';
 import {ActivatedRoute} from '@angular/router';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
     selector: 'app-product-list',
@@ -24,6 +26,7 @@ export class ProductListComponent implements OnInit {
     previousKeyword: string = '';
 
     constructor(private productService: ProductService,
+                private cartService: CartService,
                 private route: ActivatedRoute) {
     }
 
@@ -105,5 +108,12 @@ export class ProductListComponent implements OnInit {
             this.thePageSize = data.page.size;
             this.theTotalElements = data.page.totalElements;
         }
+    }
+    addToCart(theProduct : Product){
+        console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+        const  theCartItem = new CartItem(theProduct);
+
+        this.cartService.addToCart(theCartItem);
     }
 }

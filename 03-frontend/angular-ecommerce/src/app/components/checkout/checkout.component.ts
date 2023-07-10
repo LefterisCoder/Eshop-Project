ae0084d176c493b257a393e6039c6a61d4f39cb5
LefterisCoder <107108837+LefterisCoder@users.noreply.github.com>
2023-07-10 19:22:09 +0300

    Fixed the dropdown lists for countries and states

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {CodeVerseFormService} from "../../services/code-verse-form.service";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -14,6 +16,12 @@ export class CheckoutComponent implements OnInit {
   creditCardYears:number[] = [];
   creditCardMonths:number[] = [];
   checkoutFormGroup !: FormGroup;
+
+  countries:Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
   constructor(private formBuilder:FormBuilder,
               private codeVerseService: CodeVerseFormService) { }
 
@@ -56,7 +64,11 @@ export class CheckoutComponent implements OnInit {
     this.codeVerseService.getCreditCardYears().subscribe(data => {
       this.creditCardYears = data;
     })
+    this.codeVerseService.getCountries().subscribe(data =>{
+      this.countries = data;
+    })
   }
+
 
   copyShippingAddressToBillingAddress(event: Event) {
     if ((event.target as HTMLInputElement).checked) {
@@ -64,9 +76,12 @@ export class CheckoutComponent implements OnInit {
       const billingAddress = this.checkoutFormGroup.get('billingAddress');
       if (shippingAddress && billingAddress) {
         billingAddress.setValue(shippingAddress.value);
+        this.billingAddressStates = this.shippingAddressStates;
       }
     } else {
       this.checkoutFormGroup.get('billingAddress')?.reset();
+
+      this.billingAddressStates = [];
     }
   }
 
@@ -90,5 +105,27 @@ export class CheckoutComponent implements OnInit {
     this.codeVerseService.getCreditCardMonths(startMonth).subscribe(data => {
       this.creditCardMonths = data;
     })
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code
+    const countryName = formGroup?.value.country.name
+    console.log(`${formGroupName} country code: ${countryCode}`)
+    console.log(`${formGroupName} country name: ${countryName}`)
+
+    this.codeVerseService.getStates(countryCode).subscribe(
+        data =>{
+          if (formGroupName === 'shippingAddress') {
+            this.shippingAddressStates = data;
+          }else {
+            this.billingAddressStates = data;
+          }
+          //select first item by default
+          // @ts-ignore
+          formGroup?.get('state').setValue(data[0]);
+        }
+    );
   }
 }
